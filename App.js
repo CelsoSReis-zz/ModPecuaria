1e66c0a9a0d30cbc268a2d8ed75d258c5200842f
Celso Reis <47906151+CelsoSReis@users.noreply.github.com>
2020-08-08 23:02:43 -0300

    Modal Tela Printipal

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button, Modal } from 'react-native';
+import Entrar from './src/Entrar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      modalVisible:false
+    };
+
+    this.entrar = this.entrar.bind(this);
+    this.sair = this.sair.bind(this);
+  
+  }
+  entrar(){
+    this.setState({modalVisible: true});
+  }
+  sair(visible){
+    this.setState({modalVisible: visible});
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Button title="Entrar" onPress={ this.entrar } />
+        <Modal animationType="slide" visible={this.state.modalVisible} >
+          <Entrar fechar={ () => this.sair(false)} />
+        </Modal>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
